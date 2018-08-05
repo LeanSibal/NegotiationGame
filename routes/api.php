96276b4efc3ced1f9eca931a35bdde92865eb062
Ramil <ramilgonzales1990@gmail.com>
2018-08-05 13:41:09 +0800

    Player API.

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/player/login', "PlayerController@login");
+Route::post('/player/register', "PlayerController@register");
+Route::get('/player/resetpassword', "PlayerController@resetPassword");
+Route::get('/player/{id}', "PlayerController@getPlayerById");
+
